a1994224fd053950888e9956abf61df53e14b57f
Erdem BÜKE <erdembke@gmail.com>
2024-01-15 23:55:25 +0300

    test class ana yapi olustu, @Autowired , @MockBean eklendi. Testler yazilmaya hazir

--- src/test/java/stoktakip/market/tepe/restapi/UrunWebServisTest.java
+++ src/test/java/stoktakip/market/tepe/restapi/UrunWebServisTest.java
@@ -1,10 +1,26 @@
 package stoktakip.market.tepe.restapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest
 class UrunWebServisTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UrunService urunService;
 
     @Test
     void urunleriListele() {
