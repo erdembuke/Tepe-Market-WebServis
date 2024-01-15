bb1b0b3b346cffece15c63dbb21a7e3de37a1197
Erdem BÜKE <erdembke@gmail.com>
2024-01-16 00:05:52 +0300

    test_urunleri_listele() unit test added

--- src/test/java/stoktakip/market/tepe/restapi/UrunWebServisTest.java
+++ src/test/java/stoktakip/market/tepe/restapi/UrunWebServisTest.java
@@ -23,18 +23,33 @@ class UrunWebServisTest {
     private UrunService urunService;
 
     @Test
-    void urunleriListele() {
+    void test_urunleri_listele() throws Exception{
+        when(urunService.urunleriListele())
+                .thenReturn(List.of(
+                        // we expect mock datas we put as static list in UrunService.class
+                        new Urun("908861", "Cikolatali Gofret", "Ulker", 40, 5.50),
+                        new Urun("908862", "Kelebek Makarna", "Nuh Ankara", 500, 10.75)
+                ));
+
+        mockMvc.perform(get("/urun/"))
+                .andExpect(status().isOk())
+                // also here we expect mock datas we put as static list in UrunService.class
+                .andExpect(content().json("[{\"seriNo\":\"908861\",\"urunAdi\":\"Cikolatali Gofret\"," +
+                        "\"urunMarka\":\"Ulker\",\"urunGramaj\":40,\"urunBirimFiyat\":5.5},{\"seriNo\":\"908862\"," +
+                        "\"urunAdi\":\"Kelebek Makarna\",\"urunMarka\":\"Nuh Ankara\",\"urunGramaj\":500," +
+                        "\"urunBirimFiyat\":10.75}]"));
+                        // Got the values from Postman Get call with raw json and copy-pasted here
     }
 
     @Test
-    void urunBul() {
+    void test_urun_bul() {
     }
 
     @Test
-    void urunEkle() {
+    void test_urun_ekle() {
     }
 
     @Test
-    void urunSil() {
+    void test_urun_sil() {
     }
 }
