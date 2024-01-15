b340febca3bc107ade56e172466b0d514ab68ad3
Erdem BÜKE <erdembke@gmail.com>
2024-01-15 19:11:00 +0300

    Spring projesi oluşturuldu

---
+++ src/main/java/stoktakip/market/tepe/restapi/StokTakipRestApiApplication.java
@@ -0,0 +1,13 @@
+package stoktakip.market.tepe.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokTakipRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokTakipRestApiApplication.class, args);
+	}
+
+}
