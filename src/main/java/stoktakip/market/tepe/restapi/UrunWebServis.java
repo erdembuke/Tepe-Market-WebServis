cceb7c1c119e27e34227c9f3b7921c1c450639b6
Erdem BÜKE <erdembke@gmail.com>
2024-01-15 19:49:36 +0300

    UrunWebServis class olusturuldu ve application.properties dosyasi eklendi. Port 8082 olarak ayarlandi

---
+++ src/main/java/stoktakip/market/tepe/restapi/UrunWebServis.java
@@ -0,0 +1,14 @@
+package stoktakip.market.tepe.restapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/urun")
+public class UrunWebServis {
+
+
+}
